9ab9737fae5f33892049544725fce1e89cb69e41
LuisMDuarte95 <luis.azevedoduarte@gmail.com>
2015-12-10 11:21:11 +0000

    Scripts

--- PROJ2/Aulas/script2.sh
+++ PROJ2/Aulas/script2.sh
@@ -1,7 +1,6 @@
-/etc/init.d/networking restart 
 ifconfig eth0 up
-ifconfig eth0 172.16.20.1/24
-route add deafult gw 172.16.20.254 
+ifconfig eth0 172.16.20.254/24
+route add -net 172.16.20.0/24 gw 172.16.21.253
 route -n
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
--- PROJ2/Aulas/script4.sh
+++ PROJ2/Aulas/script4.sh
@@ -1,7 +1,8 @@
-/etc/init.d/networking restart 
 ifconfig eth0 up
-ifconfig eth0 172.16.20.1/24
+ifconfig eth0 172.16.20.254/24
 route add deafult gw 172.16.20.254 
 route -n
+ifconfig eth1 up
+ifconfig eth1 172.16.21.253/24
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
