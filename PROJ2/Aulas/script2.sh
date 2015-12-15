ccb4c92f252bee5289ce8a4e2161088623f08c73
LuisMDuarte95 <luis.azevedoduarte@gmail.com>
2015-12-15 23:37:43 +0000

    Scripts por testar

--- PROJ2/Aulas/script2.sh
+++ PROJ2/Aulas/script2.sh
@@ -1,6 +1,14 @@
 ifconfig eth0 up
-ifconfig eth0 172.16.20.254/24
+ifconfig eth0 172.16.21.1/24
 route add -net 172.16.20.0/24 gw 172.16.21.253
-route -n
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+
+route add default gw 172.16.21.254
+
+echo 0 > /proc/sys/net/ipv4/conf/eth0/accept_redirects 
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
+route del –net 172.16.20.0 gw 172.16.21.253 netmask 255.255.255.0 dev eth0
+traceroute 172.16.20.1
+route add –net 172.16.20.0 gw 172.16.21.253 netmask 255.255.255.0 dev eth0
+traceroute 172.16.20.1
+echo 1 > /proc/sys/net/ipv4/conf/eth0/accept_redirects 
+echo 1 > /proc/sys/net/ipv4/conf/all/accept_redirects
--- PROJ2/Aulas/script4.sh
+++ PROJ2/Aulas/script4.sh
@@ -1,8 +1,11 @@
 ifconfig eth0 up
 ifconfig eth0 172.16.20.254/24
 route add deafult gw 172.16.20.254 
-route -n
+arp -d 172.16.20.254
+
 ifconfig eth1 up
 ifconfig eth1 172.16.21.253/24
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+
+route add -net 172.16.20.0/24 gw 172.16.21.253
